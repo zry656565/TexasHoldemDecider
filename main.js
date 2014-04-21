81205c2fc9a37971917c015f2020867935770e58
邹润阳 <zry656565@gmail.com>
2014-04-21 21:13:21 +0800

    init the Class

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 /**
  * Created by JerryZou on 14-4-20.
  */
+
+var Suit = {
+    Spade: 0,
+    Heart: 1,
+    Diamond: 2,
+    Club: 3
+};
+
+var Card = function() {
+    this.suit = Math.floor(Math.random() * 4);
+    this.point = Math.floor(Math.random() * 13);
+};
+
+var Desk = function() {
+    this.cards = [];
+}
+
+var Player = function() {
+    this.card1 = new Card();
+    this.card2 = new Card();
+}
